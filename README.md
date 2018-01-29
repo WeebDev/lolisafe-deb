381c8f342f7eba9bdb352a381b4eea0258275bf0
RyoshiKayo <kayo@kayo.moe>
2018-01-29 00:20:31 -0500

    3.0.0 Working release

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # lolisafe-deb
-Debian Repository for PPA
+Debian Repository for the lolisafe PPA.
+
+## Installing:
+
+## Starting/Stopping:
+In this instance, lolisafe is controlled via a service file. Starting is as easy as `sudo service lolisafe start` and stopping is the same `sudo service lolisafe stop`. 
+
+The service runs under the user `lolisafe` so if something happens, you can always do `sudo killall -u lolisafe`.
+
+The logfile for any output (good or bad) will be in `/var/log/lolisafe.log`
+
+## Files and their locations:
+This debian package accesses three locations, as show below:
+
+Note: You will see duplicate of files in places, such as `/usr/share/lolisafe/config.js`. This is a symlink! To edit the config you must go to `/etc/lolisafe/config.js`
+
+### `/usr/share/lolisafe/`
+This is the main location for the files, including the main script `lolisafe.js`, `node_modules/` etc.
+### `/var/www/lolisafe/`
+This is the location for all file that are access via the web interafce. This include the actual images themselves as well as the various icons and default images.
+### `/etc/lolisafe/`
+This is the location for all configuration files for lolisafe. For configuration help, look at the lolisafe README, as it is already explained there.
---
+++ debian/lolisafe.init.d
@@ -0,0 +1,91 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          lolisafe
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Description: 	     lolisafe service script
+### END INIT INFO
+
+# Author: RyoshiKayo <kayo@kayo.moe>
+
+SCRIPT=/usr/share/lolisafe/lolisafe.js
+RUNAS=lolisafe
+
+PIDFILE=/var/run/lolisafe.pid
+LOGFILE=/var/log/lolisafe.log
+
+start() {
+	local is_running=$(pgrep -F "$PIDFILE" &> /dev/null)
+	if [ $? -ne 0 ] && [ -f "$PIDFILE" ]
+	then
+		echo "Lolisafe is already running!" >&2
+		return 1
+	fi
+	echo "Starting lolisafe..." >&2
+	echo "Starting lolisafe!" >> "$LOGFILE"
+	local CMD="cd /usr/share/lolisafe && /usr/bin/node $SCRIPT > $LOGFILE 2>&1 &"
+	su -c "$CMD" "$RUNAS" 
+	echo $! > "$PIDFILE"
+	echo "Lolisafe started! $(cat $PIDFILE)" >&2
+}
+
+stop() {
+	if [ ! -e "$PIDFILE" ]
+	then
+		echo "Lolisafe is not running (no pidfile)!" >&2
+		return 0
+	fi
+
+	local is_running=$(pgrep -F "$PIDFILE")
+	if [ $? -eq 0 ] && [ ! -f "$PIDFILE" ]
+	then
+		echo "Lolisafe is not running!" >&2
+		return 1
+	fi
+	echo "Shutting down lolisafe..." >&2
+	echo "Stopping lolisafe" >> "$LOGFILE"
+	kill -n 15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
+	echo "Lolisafe stopped" >&2
+}
+
+uninstall() {
+	echo -n "Are you really sure you want to uninstall this service? This cannot be undone. [y/n] "
+	local SURE
+	read SURE
+	if [[ ! "$SURE" =~ "y" ]]
+	then
+		echo "NOT uninstalling lolisafe service." >&2
+		return 0
+	fi
+	force_uninstall
+}
+
+force_uninstall() {
+	stop
+	rm -f "$PIDFILE"
+	update-rc.d -f lolisafe remove
+	rm -fv "$0"
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	uninstall)
+		uninstall
+		;;
+	force-uninstall)
+		force_uninstall
+		;;
+	restart)
+		start
+		stop
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart|uninstall}"
+esac
